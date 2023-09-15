a258cf9faf488b938000501a7d9640da7b74bf1a
Roman <mazaxaka.tyt@gmail.com>
2023-09-15 14:08:06 +0300

    add new form

--- app.js
+++ app.js
@@ -195,3 +195,44 @@ window.addEventListener("scroll", function () {
         cover.classList.remove("hide");
     }
 });
+
+// phone input
+
+const input = document.querySelector("#phone1");
+const output = document.querySelector("#output");
+const inputWrapper = document.querySelector(".form-phone-input");
+const formbutton = document.querySelector(".send-form-button");
+
+const iti = window.intlTelInput(input, {
+    utilsScript:
+        "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
+    // nationalMode: true,
+    hiddenInput: "phone",
+    preferredCountries: ["ca", "ua"],
+});
+
+const handleChange = () => {
+    let text;
+    if (input.value) {
+        if ((text = iti.isValidNumber())) {
+            text =
+                "Номер дійсний, повний міжнародний формат: " + iti.getNumber();
+            inputWrapper.classList.add("true-number");
+            inputWrapper.classList.remove("false-number");
+            formbutton.disabled = false;
+        } else {
+            text = "Недійсний номер";
+            inputWrapper.classList.add("false-number");
+            inputWrapper.classList.remove("true-number");
+            formbutton.disabled = true;
+        }
+    } else {
+        text = "Будь ласка, введіть номер у міжнародному форматі";
+    }
+    const textNode = document.createTextNode(text);
+    output.innerHTML = "";
+    output.appendChild(textNode);
+};
+
+input.addEventListener("change", handleChange);
+input.addEventListener("keyup", handleChange);
